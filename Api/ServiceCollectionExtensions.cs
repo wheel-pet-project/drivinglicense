6a5a222a5f747eedd49e27c3d405e565c8e81207
Nikita Berezhko <berezhko.nik16@gmail.com>
2025-01-29 22:48:51 +0300

    update ServiceCollectionExtensions.cs

--- Api/ServiceCollectionExtensions.cs
+++ Api/ServiceCollectionExtensions.cs
@@ -1,3 +1,4 @@
+using System.Data.Common;
 using Infrastructure.Adapters.Postgres;
 using Microsoft.EntityFrameworkCore;
 using Npgsql;
@@ -9,20 +10,26 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection RegisterPostgresDataContext(this IServiceCollection services, 
         IConfiguration configuration)
     {
-        var dataSourceBuilder = new NpgsqlDataSourceBuilder
+        services.AddTransient<DbDataSource, NpgsqlDataSource>(_ =>
         {
-            ConnectionStringBuilder =
+            var dataSourceBuilder = new NpgsqlDataSourceBuilder
             {
-                ApplicationName = "Driving_license#" + Environment.MachineName,
-                Host = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost",
-                Port = int.Parse(Environment.GetEnvironmentVariable("POSTGRES_PORT") ?? "5430"),
-                Database = Environment.GetEnvironmentVariable("POSTGRES_DB") ?? "drivinglicense_db",
-                Username = Environment.GetEnvironmentVariable("POSTGRES_USER") ?? "postgres",
-                Password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") ?? "password"
-            }
-        };
-        Console.WriteLine(dataSourceBuilder.ConnectionString);
-        var dataSource = dataSourceBuilder.Build(); 
+                ConnectionStringBuilder =
+                {
+                    ApplicationName = "Driving_license#" + Environment.MachineName,
+                    Host = Environment.GetEnvironmentVariable("POSTGRES_HOST") ?? "localhost",
+                    Port = int.Parse(Environment.GetEnvironmentVariable("POSTGRES_PORT") ?? "5430"),
+                    Database = Environment.GetEnvironmentVariable("POSTGRES_DB") ?? "drivinglicense_db",
+                    Username = Environment.GetEnvironmentVariable("POSTGRES_USER") ?? "postgres",
+                    Password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD") ?? "password"
+                }
+            };
+
+            return dataSourceBuilder.Build();
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+        var dataSource = serviceProvider.GetRequiredService<DbDataSource>();
         
         services.AddDbContext<DataContext>(optionsBuilder =>
         {
